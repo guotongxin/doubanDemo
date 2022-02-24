ddc925e3b0a6bf897fa80b24d46e13bc088a040e
lovesanta <1906798265@qq.com>
2022-02-24 12:51:38 +0800

    广告图轮播

--- src/views/Home/Components/Banner.vue
+++ src/views/Home/Components/Banner.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="banner">
-    <a href="javascript:;"><img src="@/assets/images/banner1.jpg" /></a>
-    <a href="javascript:;"><img src="@/assets/images/banner2.jpg" /></a>
+   <a href="javascript:;"><img src="@/assets/images/banner1.jpg"/></a>
+    <a href="javascript:;"><img src="@/assets/images/banner2.jpg"/></a>
   </div>
 </template>
 
 <script>
+import $ from 'jquery'
 export default {
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    var len = $('.banner').find('a') // 获取广告图片
+    var pos = 0 // 定义变量值为0
+    setInterval(() => {
+      len.eq(pos).hide() // 隐藏元素
+      pos++ // 变量值加1
+      if (pos === len.length) pos = 0 // 变量值重新定义为0
+      len.eq(pos).show() // 显示元素
+    }, 3000) // 每隔3秒钟切换元素
+  }
 
 }
 </script>
@@ -18,6 +33,13 @@ export default {
 }
 .banner>a:not(:first-child){
   display:none;
+}
+.isShow{
+  display: none;
+}
+.show{
+  display: block;
+
 }
 
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
+const webpack = require('webpack')
 module.exports = {
-
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      })
+    ]
+  },
   devServer: {
     proxy: {
       '/j': {
